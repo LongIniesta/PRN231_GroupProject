1d9d96a8dbae76351d6f4c566915f65438da5b95
Choids <hiep.bh.02@gmail.com>
2023-10-19 10:41:40 +0700

    1st commit

--- BE_PRN231_CatDogLover/BE_PRN231_CatDogLover/Controllers/AccountController.cs
+++ BE_PRN231_CatDogLover/BE_PRN231_CatDogLover/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Repositories.Interface;
+using Repositories;
 
 namespace BE_PRN231_CatDogLover.Controllers
 {
@@ -7,5 +10,28 @@ namespace BE_PRN231_CatDogLover.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly IConfiguration Configuration;
+        private readonly IMapper mapper;
+        private IAccountRepository accountRepository;
+        public AccountController(IConfiguration configuration, IMapper mapper)
+        {
+            Configuration = configuration;
+            this.mapper = mapper;
+            accountRepository = new AccountRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetOne(int id) 
+        {
+
+        }
+
+
     }
 }
